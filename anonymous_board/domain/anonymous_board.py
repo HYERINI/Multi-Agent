e277262cffc632a8bb675cac39424c03e0dc9d50
hyerini <rinlee2001@naver.com>
2025-11-19 11:35:14 +0900

    fix: [이혜린] Board 도메인 엔티티 수정 (author_id 추가) [AIS-4]

--- anonymous_board/domain/anonymous_board.py
+++ anonymous_board/domain/anonymous_board.py
@@ -2,8 +2,9 @@ from typing import Optional
 from datetime import datetime
 
 class AnonymousBoard:
-    def __init__(self, title: str, content: str):
+    def __init__(self, title: str, content: str, author_id: Optional[int] = None):
         self.id: Optional[int] = None
+        self.author_id: Optional[int] = author_id #None이면 완전 익명
         self.title = title
         self.content = content
         self.created_at: datetime = datetime.utcnow()
